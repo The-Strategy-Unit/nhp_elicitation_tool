6eff33fa5e4248670340bf07f34043ad424f1dc0
Tom Jemmett <thomas.jemmett@nhs.net>
2023-08-15 15:29:11 +0100

    initial commit

---
+++ R/app_ui.R
@@ -0,0 +1,55 @@
+#' The application User-Interface
+#'
+#' @param request Internal parameter for `{shiny}`.
+#'     DO NOT REMOVE.
+#' @noRd
+app_ui <- function(request) {
+  header <- bs4Dash::dashboardHeader(
+    title = bs4Dash::bs4DashBrand(
+      title = "NHP Elicitation Tool",
+      image = "https://the-strategy-unit.github.io/assets/logo_yellow.svg"
+    )
+  )
+
+  sidebar <- bs4Dash::bs4DashSidebar()
+
+  body <- bs4Dash::bs4DashBody()
+
+  page <- bs4Dash::bs4DashPage(
+    title = "NHP Elicitation Tool",
+    header = header,
+    sidebar = sidebar,
+    body = body
+  )
+
+
+  shiny::tagList(
+    # Leave this function for adding external resources
+    golem_add_external_resources(),
+    # Your application UI logic
+    page
+  )
+}
+
+#' Add external Resources to the Application
+#'
+#' This function is internally used to add external
+#' resources inside the Shiny application.
+#'
+#' @noRd
+golem_add_external_resources <- function() {
+  golem::add_resource_path(
+    "www",
+    app_sys("app/www")
+  )
+
+  shiny::tags$head(
+    golem::favicon(),
+    golem::bundle_resources(
+      path = app_sys("app/www"),
+      app_title = "."
+    )
+    # Add here other external resources
+    # for example, you can add shinyalert::useShinyalert()
+  )
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# NHP Elicitation Tool
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+Shiny app for the NHP National Elicitation Project.
