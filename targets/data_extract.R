55124a8321fa6782ef1281299bce851f5029ba50
Tom Jemmett <thomas.jemmett@nhs.net>
2023-08-18 16:32:25 +0100

    adds extra data extraction steps
    * extracts england population data
    * extracts denominator for preop los
    * calculates the rates data

--- targets/data_extract.R
+++ targets/data_extract.R
@@ -72,6 +72,79 @@ get_england_pop <- function() {
   filename
 }
 
+get_preop_los_denominator <- function(fyear) {
+  con <- db_con()
+
+  tbl_ip <- dplyr::tbl(con, dbplyr::in_schema("nhp_modelling", "inpatients"))
+  tbl_procs <- dplyr::tbl(con, "tbInpatientsProcedures") |>
+    dplyr::select(-"FYEAR")
+
+  tbl_ip |>
+    dplyr::inner_join(tbl_procs, by = dplyr::join_by("EPIKEY")) |>
+    dplyr::filter(
+      .data[["FYEAR"]] == fyear,
+      .data[["ADMIMETH"]] %LIKE% "1%",
+      !.data[["OPCODE"]] %LIKE% "[UYZ]%",
+      dplyr::between(
+        .data[["OPDATE"]],
+        .data[["ADMIDATE"]],
+        .data[["DISDATE"]]
+      )
+    ) |>
+    dplyr::count() |>
+    dplyr::collect() |>
+    dplyr::mutate(fyear = fyear)
+}
+
+get_rates <- function(values, england_pop, preop_los_denominator) {
+  fyears <- unique(values$FYEAR)
+
+  pop <- england_pop |>
+    readr::read_csv(col_types = "ii") |>
+    dplyr::transmute(
+      fyear = year_to_fyear(.data[["year"]]),
+      n = .data[["pop"]]
+    ) |>
+    dplyr::filter(fyear %in% fyears)
+
+  denominators <- dplyr::bind_rows(
+    .id = "strategy",
+    "alcohol_partially_attributable-acute" = pop,
+    "alcohol_partially_attributable-chronic" = pop,
+    "alcohol_wholly_attributable" = pop, 
+    "cancelled_operations" = pop,
+    "emergency_elderly" = pop,
+    "eol_care-0-2_days" = pop,
+    "eol_care-3+_days" = pop,
+    "frail_elderly-high" = pop,
+    "frail_elderly-intermediate" = pop,
+    "frail_elderly-low" = pop,
+    "intentional_self_harm" = pop,
+    "medically_unexplained_related_admissions" = pop, 
+    "obesity_related_admissions" = pop,
+    "preop_los-1day" = preop_los_denominator,
+    "preop_los-2day" = preop_los_denominator,
+    "raid_ae" = pop,
+    "raid_ip" = pop, 
+    "readmission_within_28_days" = pop,
+    "smoking" = pop,
+    "stroke_early_supported_discharge" = pop,
+    "zero_los_no_procedure-adult" = pop,
+    "zero_los_no_procedure-child" = pop
+  ) |>
+    dplyr::rename(d = "n")
+
+  filename <- "inst/app/data/trend_data.csv"
+
+  values |>
+    dplyr::rename(year = "FYEAR") |>
+    dplyr::inner_join(denominators, by = c("strategy", "year" = "fyear")) |>
+    dplyr::mutate(rate = .data[["n"]] / .data[["d"]]) |>
+    readr::write_csv(filename)
+  
+  filename
+}
+
 
       # targets::tar_read(values) |>
       #   dplyr::rename(fyear = "FYEAR") |>
