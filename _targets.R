7469160c274021918caa0bc590f218927e41f378
Tom Jemmett <thomas.jemmett@nhs.net>
2023-08-18 16:32:25 +0100

    adds extra data extraction steps
    * extracts england population data
    * extracts denominator for preop los
    * calculates the rates data

--- _targets.R
+++ _targets.R
@@ -4,6 +4,11 @@ tar_source("targets")
 
 list(
   tar_target(
+    england_pop,
+    get_england_pop(),
+    format = "file"
+  ),
+  tar_target(
     strategies,
     get_strategies()
   ),
@@ -15,5 +20,15 @@ list(
     values,
     get_values(strategies, fyears),
     pattern = cross(strategies, fyears)
+  ),
+  tar_target(
+    preop_los_denominator,
+    get_preop_los_denominator(fyears),
+    pattern = map(fyears)
+  ),
+  tar_target(
+    rates,
+    get_rates(values, england_pop, preop_los_denominator),
+    format = "file"
   )
 )
