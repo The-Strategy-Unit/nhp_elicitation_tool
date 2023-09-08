b478058f3263c9b983bd3ed8845711b4f2360de8
Tom Jemmett <thomas.jemmett@nhs.net>
2023-09-08 15:21:38 +0100

    adds app title

--- R/app_ui.R
+++ R/app_ui.R
@@ -54,9 +54,7 @@ golem_add_external_resources <- function() {
     golem::favicon(),
     golem::bundle_resources(
       path = app_sys("app/www"),
-      app_title = "."
+      app_title = "NHP National Elicitation Tool"
     )
-    # Add here other external resources
-    # for example, you can add shinyalert::useShinyalert()
   )
 }
