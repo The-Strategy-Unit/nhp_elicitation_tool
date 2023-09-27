8a09a0c4bc963b95841d7f36d9a2adf10581f3a0
Tom Jemmett <thomas.jemmett@nhs.net>
2023-09-27 20:43:19 +0100

    adds home module
    provides a way to load only the mitigators the user is interested in

--- R/app_server.R
+++ R/app_server.R
@@ -4,5 +4,19 @@
 #'     DO NOT REMOVE.
 #' @noRd
 app_server <- function(input, output, session) {
-  mod_mitigator_server("mitigator")
+  home <- mod_home_server("home")
+
+  email <- shiny::reactive(home()$email)
+  strategies <- shiny::reactive(home()$strategies)
+
+  mod_mitigator_server("mitigator", email, strategies)
+
+  shiny::observe({
+    shiny::updateTabsetPanel(
+      session,
+      "tabset",
+      "tab_mitigator"
+    )
+  }) |>
+    shiny::bindEvent(strategies())
 }
