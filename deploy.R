24726657fcc7b9a5725a223f76d422e5966b6abf
Tom Jemmett <thomas.jemmett@nhs.net>
2023-09-29 17:00:32 +0100

    updates deployment

--- deploy.R
+++ deploy.R
@@ -1,11 +1,18 @@
-rsconnect::deployApp(
-  appFiles = c(
-    "DESCRIPTION",
-    "NAMESPACE",
-    fs::dir_ls("R"),
-    "app.R",
-    fs::dir_ls("inst", recurse = TRUE)
-  ),
-  # appName = "nhp_elicitation_tool_prod"
-  appName = "nhp_elicitation_tool_dev"
-)
+deploy <- function(name) {
+  rsconnect::deployApp(
+    appFiles = c(
+      "DESCRIPTION",
+      "NAMESPACE",
+      fs::dir_ls("R"),
+      "app.R",
+      fs::dir_ls("inst", recurse = TRUE)
+    ),
+    appName = name
+  )
+}
+
+# prod
+deploy("nhp_elicitation_tool_prod")
+
+# dev
+deploy("nhp_elicitation_tool")
