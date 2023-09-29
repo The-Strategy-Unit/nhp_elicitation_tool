0d3c14ce0e5e6af33df41075aec8db30c5784bc6
Tom Jemmett <thomas.jemmett@nhs.net>
2023-09-29 15:15:08 +0100

    adds text for the home page

--- R/mod_home_ui.R
+++ R/mod_home_ui.R
@@ -10,17 +10,29 @@ mod_home_ui <- function(id) {
   ns <- shiny::NS(id)
   shiny::tagList(
     shiny::tags$h1("NHP National Elicitation Tool"),
-    shiny::tags$p("TEXT TO GO HERE"),
-    shiny::textInput(
-      ns("email"),
-      "Email"
-    ),
-    shiny::textOutput(
-      ns("selected_mitigators")
-    ),
-    shiny::actionButton(
-      ns("start"),
-      "Start"
+    shiny::fluidRow(
+      bs4Dash::box(
+        title = "Enter your email",
+        collapsible = FALSE,
+        width = 4,
+        shiny::textInput(
+          ns("email"),
+          "Email"
+        ),
+        shiny::textOutput(
+          ns("selected_mitigators")
+        ),
+        shiny::actionButton(
+          ns("start"),
+          "Start"
+        )
+      ),
+      bs4Dash::box(
+        title = "Instructions",
+        collapsible = FALSE,
+        width = 8,
+        md_file_to_html("app", "home_text.md")
+      )
     )
   )
 }
