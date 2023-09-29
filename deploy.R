f38cfebf4d317d521b49d691ac3d71d8e48debe4
Tom Jemmett <thomas.jemmett@nhs.net>
2023-09-29 16:39:11 +0100

    reverts to deploying to dev

--- deploy.R
+++ deploy.R
@@ -5,5 +5,7 @@ rsconnect::deployApp(
     fs::dir_ls("R"),
     "app.R",
     fs::dir_ls("inst", recurse = TRUE)
-  )
+  ),
+  # appName = "nhp_elicitation_tool_prod"
+  appName = "nhp_elicitation_tool_dev"
 )
